7d4054b74d2b12140805528722f07010372eb1cc
Javier Soto <sotoseattle@gmail.com>
2015-10-22 18:44:53 -0700

    a bit more ch2

--- sotoseattle/ch2.rkt
+++ sotoseattle/ch2.rkt
@@ -138,8 +138,20 @@
 
 (test-true "." (is-zerod? (diff (one) (one))))
 
-(define firstD (lambda (t) (car (cdr t)) ))
-(define seconD (lambda (t) (car (cdr (cdr t)))))
+;(define firstD (lambda (t) (car (cdr t))))
+;(define seconD (lambda (t) (car (cdr (cdr t)))))
+(define firstD
+  (lambda (t)
+    (cond
+      [(equal? t '(one)) '(one)]
+      [(car (cdr t))])))
+
+(define seconD
+  (lambda (t)
+    (cond
+      [(equal? t '(one)) (zerod)]
+      [(car (cdr (cdr t)))])))
+
 
 (define to-intd
   (lambda (t)
@@ -183,6 +195,384 @@
 
 (test-equal? "." (to-intd (successord (successord (successord 
                            (predecessord (predecessord (predecessord (zerod)))))))) 0)
+
+(define plusd_slow ; the same as plus => very slow because succ adds too much blabber
+  (lambda (b1 b2)
+    (cond
+      [(is-zerod? b1) b2]
+      [(plusd_slow (predecessord b1) (successord b2))])))
+
+(test-equal? "." (plusd_slow (zerod) (zerod)) (zerod))
+
+(define diff-tree-plus ; needs refactor and more thought
+  (lambda (b1 b2)
+    (cond
+      [(is-zerod? b1) b2]
+      [(is-zerod? b2) b1]
+      [(equal? b1 '(one)) (diff (successord (firstD b2)) (seconD b2))]
+      [(equal? b2 '(one)) (diff (successord (firstD b1)) (seconD b1))]
+      [(diff (diff-tree-plus (firstD b1) (firstD b2))
+             (diff-tree-plus (seconD b1) (seconD b2)))])))
+
+(test-equal? "." (to-intd (diff-tree-plus (zerod) (zerod))) 0)
+(test-equal? "." (to-intd (diff-tree-plus (one) (one))) 2)
+(test-equal? "." (to-intd (diff-tree-plus (one) 
+                                 (diff (diff (one) (diff (one) (one)))
+                                       (diff (diff (one) (one)) (diff (one) (zerod)))))) 3)
+(test-equal? "." (to-intd (diff-tree-plus (diff (diff (one) (diff (one) (one)))
+                                       (diff (diff (one) (one)) (diff (one) (zerod))))
+                                 (one))) 3)
+(test-equal? "." (to-intd (diff-tree-plus (zerod) (diff (one) (diff (one) (one))))) 1)
+(test-equal? "." (to-intd (diff-tree-plus (diff (one) (diff (one) (one))) (zerod))) 1)
+(test-equal? "." (to-intd (diff-tree-plus (diff (one) (diff (one) (one)))
+                                 (diff (one) (diff (one) (one))))) 2)
+(test-equal? "." (to-intd (diff-tree-plus
+                           (diff (diff (one) (diff (one) (one)))
+                                 (diff (diff (one) (one)) (diff (one) (zerod))))                 ; 2
+                           (predecessord (predecessord (predecessord (diff (one) (one))))))) -1) ; -3
+
+; 2.4 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define empty-env
+  (lambda ()
+    '(empty-env)))
+
+(define extend-env
+  (lambda (var val env)
+    (list 'extend-env var val env)))
+
+(define report-not-found (lambda () (eopl:error 'apply-env "No binding found")))
+(define report-invalid-env (lambda () (eopl:error 'apply-env "Bad Environs")))
+
+(define apply-env
+  (lambda (env var)
+    (cond
+      [(eqv? (car env) '(empty-env)) (report-not-found)]
+      [(eqv? (car env) 'extend-env)
+       (let ([saved-var (car (cdr env))]
+             [saved-val (car (cdr (cdr env)))]
+             [saved-env (car (cdr (cdr (cdr env))))])
+         (cond
+           [(eqv? var saved-var) saved-val]
+           [(apply-env saved-env var)]))]
+      [else (report-invalid-env)])))
+
+(define e
+  (extend-env 'd 6
+    (extend-env 'y 8
+      (extend-env 'x 7
+        (extend-env 'y 14
+          (empty-env))))))
+
+(test-equal? "." (apply-env e 'x) 7)
+(test-equal? "." (apply-env e 'y) 8)
+(check-exn exn:fail? (lambda () (apply-env e 'z)))
+
+; I am not sure they are asking for this...
+
+(define empty-stack     ; constructor
+  (lambda ()
+    '(empty-stack)))
+
+(define push            ; constructor
+  (lambda (val stk)
+    (list 'push val stk)))
+
+(define report-end-of-stack  (lambda () (eopl:error 'apply-env "Enf Of Stack")))
+(define report-invalid-stack (lambda () (eopl:error 'apply-env "Bad Stack")))
+
+(define empty-stack?    ; observer
+  (lambda (stk)
+    (equal? stk '(empty-stack))))
+
+(define top             ; observer
+  (lambda (stk)
+    (cond
+      [(empty-stack? stk) (report-end-of-stack)]
+      [(equal? (car stk) 'push) (car (cdr stk))]
+      [else (report-invalid-stack)])))
+
+(define pull            ; constructor
+  (lambda (stk)
+    (cond
+      [(empty-stack? stk) (report-end-of-stack)]
+      [(equal? (car stk) 'push) (car (cdr (cdr stk)))]
+      [else (report-invalid-stack)])))
+
+(define s
+  (push 6
+    (push 8
+      (push 7
+        (push 14
+          (empty-stack))))))
+
+(test-false  "." (empty-stack? s))
+(test-equal? "." (top s) 6)
+(test-equal? "." (top (pull s)) 8)
+(test-true   "." (empty-stack? (pull (pull (pull (pull s))))))
+
+; 2.5 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define empty-env-al
+  (lambda ()
+    '()))
+
+(define empty-env-al?
+  (lambda (e)
+    (null? e)))
+
+(define extend-env-al
+  (lambda (var val env)
+    (cons (list var val) env)))
+
+(define apply-env-al
+  (lambda (env var)
+    (cond
+      [(empty-env-al? env) (report-not-found)]
+      [(equal? (car (car env)) var) (car (cdr (car env)))]
+      [(apply-env-al (cdr env) var)])))
+
+(define eal
+  (extend-env-al 'd 6
+    (extend-env-al 'y 8
+      (extend-env-al 'x 7
+        (extend-env-al 'y 14
+          (empty-env-al))))))
+
+(test-equal? "." (apply-env-al eal 'y) 8)
+(check-exn exn:fail? (lambda () (apply-env-al eal 'z)))
+
+; 2.6 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;     the idea is to play between different data structures and nature of (var/val) to store
+
+; First Idea: Use numbers to store numbers
+
+(define empty-env-1 (lambda () 1))
+(define empty-env-1? (lambda (e) (equal? e 1)))
+
+(define extend-env-1
+  (lambda (n env)
+    (* env n)))
+
+(define apply-env-1
+  (lambda (env n)
+    (and (not (empty-env-1? env)) (equal? (modulo env n) 0))))
+
+(define e1
+  (extend-env-1 2
+    (extend-env-1 3
+      (extend-env-1 5
+        (extend-env-1 13
+          (empty-env-1))))))
+
+(test-true  "." (apply-env-1 e1 2))
+(test-false "." (apply-env-1 e1 7))
+
+; Second Idea: List with recursive reference
+;   todo: fail because of circularity not enforced !!
+;   todo: the order of extensions matter !!
+
+(define empty-env-2 (lambda () '()))
+(define empty-env-2? (lambda (e) (null? e)))
+
+(define fetch
+  (lambda (var env)
+    (cond
+      [(null? env) env]
+      [(equal? var (car (car env)))
+       (let ([vl (car (cdr (car env)))])
+         (if (symbol? vl)
+             (fetch vl env)
+             vl))]
+      [(fetch var (cdr env))])))
+
+(test-equal? "." (fetch 'a '((a 1) (x c) (c 3))) 1)
+(test-equal? "." (fetch 'x '((a 1) (x c) (c 3))) 3)
+(test-equal? "." (fetch 'b '((a 1) (x c) (c 3))) '())
+
+(define extend-env-2
+  (lambda (var val env)
+    (if (null? (fetch var env))
+        (cons (list var val) env)
+        env)))
+
+(define apply-env-2
+  (lambda (env var)
+    (let ([eol (fetch var env)])
+      (if (null? eol)
+          (report-not-found)
+          eol))))
+
+(define e2
+  (extend-env-2 'd 10
+    (extend-env-2 'w 'y            
+      (extend-env-2 'y 'x
+        (extend-env-2 'x 'z
+          (extend-env-2 'z 9
+            (extend-env-2 'z 1
+              (empty-env-2))))))))
+
+(test-equal? "." (apply-env-2 e2 'z) 1)
+(test-equal? "." (apply-env-2 e2 'x) 1)
+(test-equal? "." (apply-env-2 e2 'w) 1)
+(check-exn exn:fail? (lambda () (apply-env-2 e2 'm)))
+
+; Third Idea: Trees // Vectors // Lambda Calculus
+
+
+; 2.7 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define report-not-found-better
+  (lambda (var env)
+    (eopl:error 'apply-env "End of Line, ~s nowhere to be found in ~s" var env)))
+
+(define report-invalid-env-better
+  (lambda (instr env)
+    (eopl:error 'apply-env "WTF? Dunno what ~s is in the environment ~s" instr env)))
+    
+
+(define apply-env-better
+  (lambda (env var)
+    (cond
+      [(eqv? (car env) '(empty-env)) (report-not-found var env)]
+      [(eqv? (car env) 'extend-env)
+       (let ([saved-var (car (cdr env))]
+             [saved-val (car (cdr (cdr env)))]
+             [saved-env (car (cdr (cdr (cdr env))))])
+         (cond
+           [(eqv? var saved-var) saved-val]
+           [(apply-env saved-env var)]))]
+      [else (report-invalid-env (car env) env)])))
+
+; 2.8 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;     already done in 2.5
+
+; 2.9 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;     already done more extensively in 2.6 - second idea as (fetch
+
+(define has-binding?
+  (lambda (var env)
+    (cond
+      [(null? env) #f]
+      [(equal? var (car (car env))) #t]
+      [(has-binding? var (cdr env))])))
+
+; 2.10 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define extend-env-al*
+  (lambda (lvar lval env)
+    (cond
+      [(null? lvar) env]
+      [(extend-env-al (car lvar) (car lval) (extend-env-al* (cdr lvar) (cdr lval) env))])))
+
+(define e10
+  (extend-env-al* '(a b c) '(1 2 3)
+    (extend-env-al 'm 5
+      (extend-env-al* '(x y) '(8 9)
+        (empty-env-al)))))
+
+(test-equal? "." e10 '((a 1) (b 2) (c 3) (m 5) (x 8) (y 9)))
+
+; 2.11 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define empty-env-alribs  (lambda () '()))
+(define empty-env-alribs? (lambda (e) (null? e)))
+
+(define extend-env-alribs  (lambda ( var  val env) (cons (list var val) env)))
+(define extend-env-alribs* (lambda (lvar lval env) (extend-env-alribs lvar lval env)))
+
+(define has-binding-ribs?
+  (lambda (var l1 l2)
+    (cond
+      [(null? l1) #f]
+      [(equal? var (car l1)) (car l2)]
+      [else (has-binding-ribs? var (cdr l1) (cdr l2))])))
+
+(define apply-env-alribs
+  (lambda (env var)
+    (if (empty-env-alribs? env) (report-not-found)
+        (let ([vars (caar env)]
+              [vals (car (cdr (car env)))])
+          (cond
+            [(list? vars)
+             (or (has-binding-ribs? var vars vals)
+                 (apply-env-alribs (cdr env) var))]
+            [(equal? vars var) vals]
+            [(apply-env-alribs (cdr env) var)])))))
+
+(define ealribs
+  (extend-env-alribs 'foo 6
+    (extend-env-alribs* '(a b c) '(1 2 3)
+      (extend-env-alribs 'x 7
+        (extend-env-alribs* '(y z) '(8 9)
+          (empty-env-alribs))))))
+
+(test-equal? "." ealribs '((foo 6) ((a b c) (1 2 3)) (x 7) ((y z) (8 9))))
+(test-equal? "." (apply-env-alribs ealribs 'foo) 6)
+(test-equal? "." (apply-env-alribs ealribs 'z) 9)
+(test-equal? "." (apply-env-alribs ealribs 'x) 7)
+(test-equal? "." (apply-env-alribs ealribs 'z) 9)
+(check-exn exn:fail? (lambda () (apply-env-al eal 'w)))
+
+; 2.12 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define empty-e
+  (lambda ()
+    (lambda (search-var)
+      (report-not-found))))
+
+(define extend-e
+  (lambda (var val e)
+    (lambda (search-var)
+      (if (equal? search-var var)
+          val
+          (apply-e e search-var)))))
+
+(define apply-e
+  (lambda (e search-var)
+    (e search-var)))
+
+(define E
+  (extend-e 'a 1
+    (extend-e 'b 2
+      (extend-e 'c 3
+        (empty-e)))))
+
+(test-equal? "." (E 'b) 2)
+
+; Again not sure if this is correct ;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define report-end-of-stK  (lambda () (eopl:error 'whathaveyou "Enf Of Stack")))
+
+(define empty-stK
+  (lambda ()
+    (lambda ()
+      (report-end-of-stK))))
+
+(define pushK
+  (lambda (saved-val saved-stk)
+    (lambda ()
+      (pullK saved-val)
+      ;saved-stk               ; if i want to return stack to keep screwing with it!
+      )))
+
+(define pullK
+  (lambda (v)
+    v))
+
+(define S
+  (pushK 6
+    (pushK 8
+      (pushK 99
+        (empty-stK)))))
+
+(define SE
+  (empty-stK))
+
+(test-equal? "." (S) 6)
+(check-exn exn:fail? (lambda () (SE)))
+
+
 
 
 
